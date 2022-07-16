7fa526faa8cb1cde7e798674871ffc5ff529cbcb
jiara <ji.arakani@gmail.com>
2022-07-16 16:53:44 -0400

    Fixed gameboard, food generation

--- snake.js
+++ snake.js
@@ -1,12 +1,37 @@
-// Next version to utilize Canvas API
+/* Next version to utilize Canvas API */
+
+// Declare globals to track gameboard, score, snake, food
 const LINE_PIXEL_COUNT = 40;
 const TOTAL_PIXEL_COUNT = LINE_PIXEL_COUNT ** 2;
+// ...to track score...
 let totalFoodEaten = 0;
 let totalDistanceTraveled = 0;
+let foodIndex = 0;
+// Reference to gameboard element
 const gameContainer = document.getElementById('game-container');
-
-const createGameBoardPixels = () => {
+// Generate gameboard
+const createGameboardPixels = () => {
     for (let i = 1; i <= TOTAL_PIXEL_COUNT; i++) {
-        gameContainer.innerHTML = `${gameContainer.innerHTML} <div class="gameboard-pixel id="pixel-${i}"></div>`
+        let pixel = document.createElement('div');
+        pixel.className = 'gameboard-pixel';
+        pixel.id = `pixel-${i}`;
+        gameContainer.appendChild(pixel);
     }
+};
+// Reference to all gameboard pixel elements
+const gameboardPixels = document.getElementsByClassName('gameboard-pixel');
+// Create randomly generated food items on gameboard
+const createFood = () => {
+    console.log(gameboardPixels[foodIndex].classList)
+    if (gameboardPixels[foodIndex].classList.contains("food")) {
+        gameboardPixels[foodIndex].classList.remove("food")
+    }
+    foodIndex = Math.floor(TOTAL_PIXEL_COUNT * Math.random())
+    gameboardPixels[foodIndex].classList.add('food')
 }
+
+/* Snake behavior */
+const LEFT_DIR = 37
+const UP_DIR = 38
+const RIGHT_DIR = 39
+const DOWN_DIR = 40
